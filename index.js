96d02f09f26f620e5f718cb2cb9ac1ef07217c44
Your Name <you@example.com>
2022-02-24 16:50:22 +0700

    REFACTORING TO MVC

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const express = require("express");
-const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
+
+const newPostController = require("./controllers/newPost");
+const homeController = require("./controllers/home");
+const storePostController = require("./controllers/storePost");
+const getPostController = require("./controllers/getPost");
+const validateMiddleWare = require("./middleware/validateMiddleWare");
+
+mongoose.connect(
+  "mongodb+srv://judasdev:judasdev2533@cluster0.b4it7.mongodb.net/node-blog-db?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
 
 const app = new express();
-const ejs = require("ejs");
+
+app.use("/posts/store", validateMiddleWare);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-app.get("/contact", (req, res) => {
-  res.render("contact");
-});
-app.get("/post", (req, res) => {
-  res.render("post");
-});
+app.get("/", homeController);
+app.get("/post/:id", getPostController);
+app.get("/posts/new", newPostController);
+app.post("/posts/store", storePostController);
 
 app.listen(4000, () => {
   console.log("App listening on port 4000");
